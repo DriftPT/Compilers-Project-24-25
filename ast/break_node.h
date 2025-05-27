7b4593b2b5a94ecd8431a0de2b154f31577c2467
Francisco Nascimento <franciscomcnascimento@tecnico.ulisboa.pt>
2025-05-27 18:00:24 +0100

    Refactor break_node and continue_node classes to remove unused _depth member

--- ast/break_node.h
+++ ast/break_node.h
@@ -8,14 +8,11 @@ namespace udf {
    * Class for describing break nodes.
    */
   class break_node: public cdk::basic_node {
-    int _depth;
 
   public:
-    break_node(int lineno, int depth = 1) :
-        cdk::basic_node(lineno), _depth(depth) {
+    break_node(int lineno) :
+        cdk::basic_node(lineno) {
     }
-
-    int depth() const { return _depth; }
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_break_node(this, level); }
 
--- ast/continue_node.h
+++ ast/continue_node.h
@@ -8,14 +8,11 @@ namespace udf {
    * Class for describing continue nodes.
    */
   class continue_node: public cdk::basic_node {
-    int _depth;
-
+    
   public:
-    continue_node(int lineno, int depth = 1) :
-        cdk::basic_node(lineno), _depth(depth) {
+    continue_node(int lineno) :
+        cdk::basic_node(lineno) {
     }
-
-    int depth() const { return _depth; }
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_continue_node(this, level); }
 
