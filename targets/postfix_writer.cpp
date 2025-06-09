05de9fbe91067b083550a3a4d07cb93e76f9a383
Pedro Macedo <pedrosantosmacedo@tecnico.ulisboa.pt>
2025-06-09 18:30:50 +0100

    Basic postfix visitors implementation

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -1,8 +1,13 @@
 #include <string>
 #include <sstream>
+#include <memory>
+#include <cdk/types/types.h>
 #include "targets/type_checker.h"
 #include "targets/postfix_writer.h"
+#include "targets/frame_size_calculator.h"
+#include "targets/symbol.h"
 #include ".auto/all_nodes.h"  // all_nodes.h is automatically generated
+#include "udf_parser.tab.h"
 
 //---------------------------------------------------------------------------
 
@@ -13,16 +18,39 @@ void udf::postfix_writer::do_data_node(cdk::data_node * const node, int lvl) {
   // EMPTY
 }
 void udf::postfix_writer::do_double_node(cdk::double_node * const node, int lvl) {
-  // EMPTY
+  if (_inFunctionBody) {
+    _pf.DOUBLE(node->value()); // load number to the stack
+  } else {
+    _pf.SDOUBLE(node->value());    // double is on the DATA segment
+  }
 }
 void udf::postfix_writer::do_not_node(cdk::not_node * const node, int lvl) {
-  // EMPTY
+  ASSERT_SAFE_EXPRESSIONS;
+  node->argument()->accept(this, lvl + 2);
+  _pf.INT(0);
+  _pf.EQ();
 }
 void udf::postfix_writer::do_and_node(cdk::and_node * const node, int lvl) {
-  // EMPTY
+  ASSERT_SAFE_EXPRESSIONS;
+  int lbl = ++_lbl;
+  node->left()->accept(this, lvl + 2);
+  _pf.DUP32();
+  _pf.JZ(mklbl(lbl));
+  node->right()->accept(this, lvl + 2);
+  _pf.AND();
+  _pf.ALIGN();
+  _pf.LABEL(mklbl(lbl));
 }
 void udf::postfix_writer::do_or_node(cdk::or_node * const node, int lvl) {
-  // EMPTY
+  ASSERT_SAFE_EXPRESSIONS;
+  int lbl = ++_lbl;
+  node->left()->accept(this, lvl + 2);
+  _pf.DUP32();
+  _pf.JNZ(mklbl(lbl));
+  node->right()->accept(this, lvl + 2);
+  _pf.OR();
+  _pf.ALIGN();
+  _pf.LABEL(mklbl(lbl));
 }
 
 //---------------------------------------------------------------------------
@@ -34,27 +62,38 @@ void udf::postfix_writer::do_sequence_node(cdk::sequence_node * const node, int 
 }
 
 void udf::postfix_writer::do_block_node(udf::block_node *const node, int lvl) {
-  // TODO
+  _symtab.push(); 
+  if (node->declarations()) node->declarations()->accept(this, lvl + 2);
+  if (node->instructions()) node->instructions()->accept(this, lvl + 2);
+  _symtab.pop();
 }
 
 //---------------------------------------------------------------------------
 
 void udf::postfix_writer::do_integer_node(cdk::integer_node * const node, int lvl) {
-  _pf.INT(node->value()); // push an integer
+  if (_inFunctionBody) {
+    _pf.INT(node->value()); // integer literal is on the stack: push an integer
+  } else {
+    _pf.SINT(node->value()); // integer literal is on the DATA segment
+  }
 }
 
 void udf::postfix_writer::do_string_node(cdk::string_node * const node, int lvl) {
   int lbl1;
-
-  /* generate the string */
-  _pf.RODATA(); // strings are DATA readonly
-  _pf.ALIGN(); // make sure we are aligned
+  /* generate the string literal */
+  _pf.RODATA(); // strings are readonly DATA
+  _pf.ALIGN(); // make sure the address is aligned
   _pf.LABEL(mklbl(lbl1 = ++_lbl)); // give the string a name
   _pf.SSTRING(node->value()); // output string characters
-
-  /* leave the address on the stack */
-  _pf.TEXT(); // return to the TEXT segment
-  _pf.ADDR(mklbl(lbl1)); // the string to be printed
+  if (_function) {
+    // local variable initializer
+    _pf.TEXT();
+    _pf.ADDR(mklbl(lbl1));
+  } else {
+    // global variable initializer
+    _pf.DATA();
+    _pf.SADDR(mklbl(lbl1));
+  }
 }
 
 //---------------------------------------------------------------------------
@@ -143,32 +182,44 @@ void udf::postfix_writer::do_eq_node(cdk::eq_node * const node, int lvl) {
 
 void udf::postfix_writer::do_variable_node(cdk::variable_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
-  // simplified generation: all variables are global
-  _pf.ADDR(node->name());
+  const std::string &id = node->name();
+  auto symbol = _symtab.find(id);
+  //external???
+  //FALTA IMPLEMENTACAO NO SYMBOL
+  /*if (symbol->global()) {
+    _pf.ADDR(symbol->name());
+  } else {
+    _pf.LOCAL(symbol->offset());
+  }*/
 }
 
 void udf::postfix_writer::do_rvalue_node(cdk::rvalue_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
   node->lvalue()->accept(this, lvl);
-  _pf.LDINT(); // depends on type size
+  //external???
+  if (node->type()->name() == cdk::TYPE_DOUBLE) {
+    _pf.LDDOUBLE();
+  } else {
+    _pf.LDINT();
+  }
 }
 
 void udf::postfix_writer::do_assignment_node(cdk::assignment_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
-  node->rvalue()->accept(this, lvl); // determine the new value
-  _pf.DUP32();
-  if (new_symbol() == nullptr) {
-    node->lvalue()->accept(this, lvl); // where to store the value
+  node->rvalue()->accept(this, lvl + 2);
+  if (node->type()->name() == cdk::TYPE_DOUBLE) {
+    if (node->rvalue()->type()->name() == cdk::TYPE_INT) _pf.I2D();
+    _pf.DUP64();
   } else {
-    _pf.DATA(); // variables are all global and live in DATA
-    _pf.ALIGN(); // make sure we are aligned
-    _pf.LABEL(new_symbol()->name()); // name variable location
-    reset_new_symbol();
-    _pf.SINT(0); // initialize it to 0 (zero)
-    _pf.TEXT(); // return to the TEXT segment
-    node->lvalue()->accept(this, lvl);  //DAVID: bah!
+    _pf.DUP32();
   }
-  _pf.STINT(); // store the value at address
+
+  node->lvalue()->accept(this, lvl);
+  if (node->type()->name() == cdk::TYPE_DOUBLE) {
+    _pf.STDOUBLE();
+  } else {
+    _pf.STINT();
+  }
 }
 
 //---------------------------------------------------------------------------
@@ -206,38 +257,41 @@ void udf::postfix_writer::do_program_node(udf::program_node * const node, int lv
 
 void udf::postfix_writer::do_evaluation_node(udf::evaluation_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
-  node->argument()->accept(this, lvl); // determine the value
-  if (node->argument()->is_typed(cdk::TYPE_INT)) {
-    _pf.TRASH(4); // delete the evaluated value
-  } else if (node->argument()->is_typed(cdk::TYPE_STRING)) {
-    _pf.TRASH(4); // delete the evaluated value's address
-  } else {
-    std::cerr << "ERROR: CANNOT HAPPEN!" << std::endl;
-    exit(1);
-  }
+  node->argument()->accept(this, lvl);
+  //nao percebo bem
+  _pf.TRASH(node->argument()->type()->size());
 }
 
 void udf::postfix_writer::do_print_node(udf::print_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
 
-  for (size_t i = 0; i < node->arguments()->size(); ++i) {
-    auto child = dynamic_cast<cdk::expression_node *>(node->arguments()->node(i));
+  for (size_t ix = 0; ix < node->arguments()->size(); ix++) {
+    auto child = dynamic_cast<cdk::expression_node*>(node->arguments()->node(ix));
 
+    std::shared_ptr<cdk::basic_type> etype = child->type();
     child->accept(this, lvl); // expression to print
-    if (child->is_typed(cdk::TYPE_INT)) {
+    if (etype->name() == cdk::TYPE_INT) {
+      _functions_to_declare.insert("printi");
       _pf.CALL("printi");
-      _pf.TRASH(4); // delete the printed value
-    } else if (child->is_typed(cdk::TYPE_STRING)) {
+      _pf.TRASH(4); // trash int
+    } else if (etype->name() == cdk::TYPE_DOUBLE) {
+      _functions_to_declare.insert("printd");
+      _pf.CALL("printd");
+      _pf.TRASH(8); // trash double
+    } else if (etype->name() == cdk::TYPE_STRING) {
+      _functions_to_declare.insert("prints");
       _pf.CALL("prints");
-      _pf.TRASH(4); // delete the printed value's address
+      _pf.TRASH(4); // trash char pointer
     } else {
-      std::cerr << "ERROR: CANNOT HAPPEN!" << std::endl;
-      exit(1);
+      std::cerr << "cannot print expression of unknown type" << std::endl;
+      return;
     }
+
   }
 
   if (node->newline()) {
-    _pf.CALL("println"); // print a newline
+    _functions_to_declare.insert("println");
+    _pf.CALL("println");
   }
 }
 
@@ -245,15 +299,23 @@ void udf::postfix_writer::do_print_node(udf::print_node * const node, int lvl) {
 
 void udf::postfix_writer::do_input_node(udf::input_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
-  _pf.CALL("readi");
-  _pf.LDFVAL32();
+  if (_lvalueType == cdk::TYPE_DOUBLE) {
+    _functions_to_declare.insert("readd");
+    _pf.CALL("readd");
+    _pf.LDFVAL64();
+  } else if (_lvalueType == cdk::TYPE_INT) {
+    _functions_to_declare.insert("readi");
+    _pf.CALL("readi");
+    _pf.LDFVAL32();
+  } else {
+    std::cerr << "FATAL: " << node->lineno() << ": cannot read type" << std::endl;
+    return;
+  }
 }
 
 //---------------------------------------------------------------------------
 
 void udf::postfix_writer::do_for_node(udf::for_node * const node, int lvl) {
-  /*
-  TODO
   ASSERT_SAFE_EXPRESSIONS;
   int lbl1, lbl2, lbl3;
   node->init()->accept(this, lvl);
@@ -265,31 +327,42 @@ void udf::postfix_writer::do_for_node(udf::for_node * const node, int lvl) {
   node->increment()->accept(this, lvl);
   _pf.JMP(mklbl(lbl1));
   _pf.LABEL(mklbl(lbl2));
-  */
+  //TODO
 }
 
 void udf::postfix_writer::do_break_node(udf::break_node *const node, int lvl) {
-  /*
-  TODO:
   if (_forIni.size() != 0) {
     _pf.JMP(mklbl(_forEnd.top())); // jump to for end
   } else
     error(node->lineno(), "'break' outside 'for'");
-  */
 }
 
 void udf::postfix_writer::do_continue_node(udf::continue_node *const node, int lvl) {
-  /*
-  TODO:
   if (_forIni.size() != 0) {
     _pf.JMP(mklbl(_forStep.top())); // jump to next cycle
   } else
     error(node->lineno(), "'restart' outside 'for'");
-  */
 }
 
 void udf::postfix_writer::do_return_node(udf::return_node *const node, int lvl) {
-  // TODO
+  ASSERT_SAFE_EXPRESSIONS;
+  if (_function->type()->name() != cdk::TYPE_VOID) {
+    node->retval()->accept(this, lvl + 2);
+
+    if (_function->type()->name() == cdk::TYPE_INT || _function->type()->name() == cdk::TYPE_STRING
+        || _function->type()->name() == cdk::TYPE_POINTER) {
+      _pf.STFVAL32();
+    } else if (_function->type()->name() == cdk::TYPE_DOUBLE) {
+      if (node->retval()->type()->name() == cdk::TYPE_INT) _pf.I2D();
+      _pf.STFVAL64();
+    } else {
+      std::cerr << node->lineno() << ": should not happen: unknown return type" << std::endl;
+    }
+  }
+
+  _pf.JMP(_currentBodyRetLabel);
+  _returnSeen = true;
+
 }
 
 //---------------------------------------------------------------------------
@@ -307,36 +380,57 @@ void udf::postfix_writer::do_if_node(udf::if_node * const node, int lvl) {
 
 void udf::postfix_writer::do_if_else_node(udf::if_else_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
-  int lbl1, lbl2;
+  int lbl_else, lbl_end;
   node->condition()->accept(this, lvl);
-  _pf.JZ(mklbl(lbl1 = ++_lbl));
+  _pf.JZ(mklbl(lbl_else = lbl_end = ++_lbl));
   node->thenblock()->accept(this, lvl + 2);
-  _pf.JMP(mklbl(lbl2 = ++_lbl));
-  _pf.LABEL(mklbl(lbl1));
-  node->elseblock()->accept(this, lvl + 2);
-  _pf.LABEL(mklbl(lbl1 = lbl2));
+  if (node->elseblock()) {
+    _pf.JMP(mklbl(lbl_end = ++_lbl));
+    _pf.LABEL(mklbl(lbl_else));
+    node->elseblock()->accept(this, lvl + 2);
+  }
+  _pf.LABEL(mklbl(lbl_end));
 }
 
 //---------------------------------------------------------------------------
 
 void udf::postfix_writer::do_sizeof_node(udf::sizeof_node * const node, int lvl) {
-  //TODO
+  ASSERT_SAFE_EXPRESSIONS;
+  _pf.INT(node->expression()->type()->size());
 }
 
 void udf::postfix_writer::do_nullptr_node(udf::nullptr_node * const node, int lvl) {
-  //TODO
+  ASSERT_SAFE_EXPRESSIONS; // a pointer is a 32-bit integer
+  if (_inFunctionBody) {
+    _pf.INT(0);
+  } else {
+    _pf.SINT(0);
+  }
 }
 
 void udf::postfix_writer::do_address_of_node(udf::address_of_node * const node, int lvl) {
-  //TODO
+  ASSERT_SAFE_EXPRESSIONS;
+  // since the argument is an lvalue, it is already an address
+  node->lvalue()->accept(this, lvl + 2);
 }
 
 void udf::postfix_writer::do_index_node(udf::index_node * const node, int lvl) {
-  //TODO
+  ASSERT_SAFE_EXPRESSIONS;
+
+  node->ptr()->accept(this, lvl);
+  node->index()->accept(this, lvl);
+  _pf.INT(node->type()->size()); // type size
+  _pf.MUL();                     // type size * index
+  _pf.ADD();                     // ptr + (type size * index)
 }
 
 void udf::postfix_writer::do_objects_alloc_node(udf::objects_alloc_node * const node, int lvl) {
-  //TODO
+  ASSERT_SAFE_EXPRESSIONS;
+  node->argument()->accept(this, lvl);
+  _pf.INT(3);
+  _pf.SHTL();
+  _pf.ALLOC(); // allocate
+  _pf.SP(); // put base pointer in stack
 }
 
 void udf::postfix_writer::do_function_call_node(udf::function_call_node * const node, int lvl) {
--- targets/postfix_writer.h
+++ targets/postfix_writer.h
@@ -2,6 +2,8 @@
 
 #include "targets/basic_ast_visitor.h"
 
+#include <set>
+#include <stack>
 #include <sstream>
 #include <cdk/emitters/basic_postfix_emitter.h>
 
@@ -12,14 +14,34 @@ namespace udf {
   //!
   class postfix_writer: public basic_ast_visitor {
     cdk::symbol_table<udf::symbol> &_symtab;
+
+    std::set<std::string> _functions_to_declare;
+
+    // semantic analysis
+    bool _errors, _inFunction, _inFunctionName, _inFunctionArgs, _inFunctionBody;
+    bool _inForInit;
+    bool _returnSeen; // when building a function
+    std::stack<int> _forIni, _forStep, _forEnd; // for break/repeat
+    std::stack<bool> _globals; // for deciding whether a variable is global or not
+    std::shared_ptr<udf::symbol> _function; // for keeping track of the current function and its arguments
+    int _offset; // current framepointer offset (0 means no vars defined)
+    cdk::typename_type _lvalueType;
+
+    // remember function name for resolving '@'
+    std::string _currentFunctionName;
+    std::string _currentBodyRetLabel; // where to jump when a return occurs of an exclusive section ends
+
+    // code generation
     cdk::basic_postfix_emitter &_pf;
     int _lbl;
 
   public:
-    postfix_writer(std::shared_ptr<cdk::compiler> compiler, cdk::symbol_table<udf::symbol> &symtab,
-                   cdk::basic_postfix_emitter &pf) :
-        basic_ast_visitor(compiler), _symtab(symtab), _pf(pf), _lbl(0) {
+    postfix_writer(std::shared_ptr<cdk::compiler> compiler, cdk::symbol_table<udf::symbol> &symtab, cdk::basic_postfix_emitter &pf) :
+        basic_ast_visitor(compiler), _symtab(symtab), _errors(false), _inFunction(false), _inFunctionName(false), _inFunctionArgs(
+            false), _inFunctionBody(false), _inForInit(false), _returnSeen(false), _function(nullptr), _offset(0), _lvalueType(
+            cdk::TYPE_VOID), _currentFunctionName(""), _currentBodyRetLabel(""), _pf(pf), _lbl(0) {
     }
+
 
   public:
     ~postfix_writer() {
@@ -35,6 +57,10 @@ namespace udf {
       else
         oss << "_L" << lbl;
       return oss.str();
+    }
+
+    void error(int lineno, std::string s) {
+      std::cerr << "error: " << lineno << ": " << s << std::endl;
     }
 
   public:
